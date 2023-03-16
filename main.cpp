4e837ab7e62a29acd283cfa0c7a57428c2c18a06
kellylau <kellylaup@gmail.com>
2023-03-16 16:13:16 -0400

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -2,33 +2,23 @@
 #include <fstream>
 #include "indent.h"
 #include "unindent.h"
+#include "funcs.h"
 
 int main () {
+    
+    std::string line; 
     std::fstream bad_code("bad-code.cpp"); 
     if (bad_code.is_open()) {
-        int tab = 0; 
-        std::string line;
         while (getline(bad_code, line)) { 
-            line = removeLeadingSpaces(line); 
-            int holder = tab;
-            if(line[0] == '}'){
-                holder--; 
-            }
-            for (int i = 0; i < holder; i++) {
-                std::cout << '\t'; 
-            }
-            std::cout << line << '\n';
-            tab+=countChar(line,'{'); 
-            tab-= countChar(line,'}'); 
-        }
+            indent_code(line); 
+        } 
         bad_code.close(); 
     }
 
     std::fstream bad_code1("bad-code1.cpp"); 
     if (bad_code1.is_open()) {
-        std::string line; 
         while (getline(bad_code1, line)) { 
-            std::cout << removeLeadingSpaces(line) << '\n';  
+            unindent_code(line); 
         }
         bad_code.close(); 
     }
